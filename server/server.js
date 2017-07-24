0274e5c6dedb90c76ea241eebb1ec731d31dd27c
harryanderson <harryanderson9@gmail.com>
2017-07-25 09:43:55 +1000

    Format timestamps using moment js

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-var server = http.createServer(app);
+var server = http.createServer(app); 
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('new user connected');
-
+    
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
